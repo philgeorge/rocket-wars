ca8af1ef3debba12a2a295227752bc997ea957db
Phil George <philipg@fastmail.fm>
2025-06-24 23:24:54 +1000

    version bump

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@
 /**
  * Game version - updated automatically by update-version.js
  */
-export const GAME_VERSION = '20250621-2342';
+export const GAME_VERSION = '20250624-2324';
 
 /**
  * Team/Player color definitions (2-4 players max)
